769b06bdce4e5fe69e527510343133e0a942f8d6
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-04-15 11:50:06 +0300

    "self.assertEqual -> assert"

--- test/hw2/test_task2.py
+++ test/hw2/test_task2.py
@@ -1,24 +1,23 @@
-import unittest
-
 from All_home_works.hw2.task2 import major_and_minor_elem
 
 
-class TestHW2Task2(unittest.TestCase):
-    def test_major_and_minor_elem(self):
-        input1 = [3, 2, 3]
-        output1 = 3, 2
-        self.assertEqual(output1, major_and_minor_elem(input1))
+def test_major_and_minor_elem():
+    input1 = [3, 2, 3]
+    output1 = 3, 2
+    assert output1 == major_and_minor_elem(input1)
 
-        input2 = [2, 2, 1, 1, 1, 2, 2]
-        output2 = 2, 1
-        self.assertEqual(output2, major_and_minor_elem(input2))
+    input2 = [2, 2, 1, 1, 1, 2, 2]
+    output2 = 2, 1
+    assert output2 == major_and_minor_elem(input2)
 
-    def test_major_and_minor_elem_non_most_common(self):
-        data = [1, 1, 1, 2, 2, 2, 0, 0, 0, 3]
-        output = (None, 3)
-        self.assertEqual(output, major_and_minor_elem(data))
 
-    def test_major_and_minor_elem_non_min_common(self):
-        data = [1, 1, 2, 2, 3, 3, 3, 3, 3, 3]
-        output = (3, None)
-        self.assertEqual(output, major_and_minor_elem(data))
+def test_major_and_minor_elem_non_most_common():
+    data = [1, 1, 1, 2, 2, 2, 0, 0, 0, 3]
+    output = (None, 3)
+    assert output == major_and_minor_elem(data)
+
+
+def test_major_and_minor_elem_non_min_common():
+    data = [1, 1, 2, 2, 3, 3, 3, 3, 3, 3]
+    output = (3, None)
+    assert output == major_and_minor_elem(data)
