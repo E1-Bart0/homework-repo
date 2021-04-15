4511267c49091e42a33e0277ac0a56fd915f90d8
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-04-15 11:56:29 +0300

    "self.assertEqual -> assert"

--- test/hw2/test_task5.py
+++ test/hw2/test_task5.py
@@ -1,19 +1,19 @@
 import string
-import unittest
 
 from All_home_works.hw2.task5 import custom_range
 
 
-class TestTask5(unittest.TestCase):
-    def test_custom_range_str(self):
-        self.assertEqual(
-            ["a", "b", "c", "d", "e", "f"], custom_range(string.ascii_lowercase, "g")
-        )
-        self.assertEqual(
-            ["g", "h", "i", "j", "k", "l", "m", "n", "o"],
-            custom_range(string.ascii_lowercase, "g", "p"),
-        )
-        self.assertEqual(
-            ["p", "n", "l", "j", "h"],
-            custom_range(string.ascii_lowercase, "p", "g", -2),
-        )
+def test_custom_range_str_case1():
+    assert ["a", "b", "c", "d", "e", "f"] == custom_range(string.ascii_lowercase, "g")
+
+
+def test_custom_range_str_case2():
+    assert ["g", "h", "i", "j", "k", "l", "m", "n", "o"] == custom_range(
+        string.ascii_lowercase, "g", "p"
+    )
+
+
+def test_custom_range_str_case3():
+    assert ["p", "n", "l", "j", "h"] == custom_range(
+        string.ascii_lowercase, "p", "g", -2
+    )
