019214e452cc156ef5c0031632d29c4e2aa89cab
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-04-15 11:53:22 +0300

    "self.assertEqual -> assert"

--- test/hw2/test_task4.py
+++ test/hw2/test_task4.py
@@ -1,17 +1,14 @@
-import unittest
-
 from All_home_works.hw2.task4 import cache
 
 
-class TestTask4(unittest.TestCase):
-    def test_cache(self):
-        some = 100, 200
+def test_cache():
+    some = 100, 200
 
-        cache_fun = cache(self.func)
-        val1 = cache_fun(*some)
-        val2 = cache_fun(*some)
-        self.assertEqual(val1, val2)
+    cache_fun = cache(func)
+    val1 = cache_fun(*some)
+    val2 = cache_fun(*some)
+    assert val1 == val2
 
-    @staticmethod
-    def func(a, b):
-        return (a ** b) ** 2
+
+def func(a, b):
+    return (a ** b) ** 2
