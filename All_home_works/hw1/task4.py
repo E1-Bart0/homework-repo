4c0fecdd78b59467a3396d48744f080a22f7c87a
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-04-18 09:14:03 +0300

    "Remade task4"

--- All_home_works/hw1/task4.py
+++ All_home_works/hw1/task4.py
@@ -10,23 +10,21 @@ from typing import List
 
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
-    queue = []
-    max_sum = nums[0]
+    counter = 0
+    max_sum = 0
 
-    for num in nums:
-        if len(queue) == k:
-            queue.pop(0)
-        queue.append(num)
-
-        max_sum = get_max_sum(max_sum, queue)
-
+    for index, current_num in enumerate(nums):
+        if index < k:
+            counter += current_num
+            max_sum += current_num
+        else:
+            num_k_indexes_ago = nums[index - k]
+            counter += current_num - num_k_indexes_ago
+            max_sum = _get_max_sum(counter, max_sum)
     return max_sum
 
 
-def get_max_sum(max_sum, queue):
-    queue_copy = queue.copy()
-    while len(queue_copy):
-        if sum(queue_copy) > max_sum:
-            max_sum = sum(queue_copy)
-        queue_copy.pop(0)
-    return max_sum
+def _get_max_sum(counter, max_sum):
+    if counter > max_sum:
+        max_sum = counter
+    return max_sum  # noqa: R504
--- test/hw1/test_task4.py
+++ test/hw1/test_task4.py
@@ -1,202 +1,28 @@
 from All_home_works.hw1.task4 import find_maximal_subarray_sum
 
 
-def test_find_maximal_subarray_sum__init_data():
+def test_find_maximal_subarray_sum__init_data__end_subarray():
     nums = [1, 3, -1, -3, 5, 3, 6, 7]
-
     k = 3
     result = 16
     assert result == find_maximal_subarray_sum(nums, k)
 
 
-def test_find_maximal_subarray_sum__one_max():
-    nums = [1, 3, -1, -3, -100, 1000, -100, 7]
-
-    k = 5
-    result = 1000
+def test_find_maximal_subarray_sum__middle_subarray():
+    nums = [0, 10, 10, 0]
+    k = 2
+    result = 20
     assert result == find_maximal_subarray_sum(nums, k)
 
 
-def test_find_maximal_subarray_sum__():
-    nums = [1, 100, -1, 0, 24, 432, 564, 76, 8, 8]
-
-    k = 4
-    result = 1096
-    assert result == find_maximal_subarray_sum(nums, k)
-
-
-def test_find_maximal_subarray_sum__len100():
-    nums = [
-        9,
-        -8,
-        1,
-        -8,
-        -8,
-        -6,
-        -7,
-        7,
-        6,
-        -8,
-        5,
-        -7,
-        -1,
-        -8,
-        -7,
-        -5,
-        0,
-        -4,
-        -5,
-        -8,
-        0,
-        6,
-        10,
-        6,
-        -5,
-        9,
-        -3,
-        1,
-        8,
-        4,
-        0,
-        -6,
-        -7,
-        2,
-        -3,
-        10,
-        -9,
-        -9,
-        3,
-        -9,
-        3,
-        3,
-        3,
-        -1,
-        2,
-        9,
-        -6,
-        1,
-        0,
-        -5,
-        1,
-        6,
-        -9,
-        1,
-        6,
-        -5,
-        -7,
-        -6,
-        -1,
-        10,
-        -8,
-        -3,
-        -3,
-        -5,
-        -10,
-        -6,
-        -9,
-        2,
-        -1,
-        7,
-        -3,
-        9,
-        -3,
-        -2,
-        10,
-        -3,
-        7,
-        -2,
-        -6,
-        9,
-        9,
-        9,
-        1,
-        4,
-        1,
-        10,
-        0,
-        0,
-        8,
-        -2,
-    ]
-
+def test_find_maximal_subarray_sum__start_subarray():
+    nums = [100, -10, 10, 10, 0]
     k = 3
-    result = 27
+    result = 100
     assert result == find_maximal_subarray_sum(nums, k)
 
 
-def test_find_maximal_subarray_sum__k_eq_len():
-    nums = [
-        -3,
-        -3,
-        -5,
-        -10,
-        -6,
-        -100,
-        2,
-        -1,
-        7,
-        -3,
-        9,
-        -3,
-        -2,
-        10,
-        -3,
-        7,
-        100,
-        -6,
-        9,
-        9,
-        9,
-        1,
-        4,
-        1,
-        10,
-        0,
-        0,
-        8,
-    ]
-
-    k = len(nums)
-    result = sum(
-        [2, -1, 7, -3, 9, -3, -2, 10, -3, 7, 100, -6, 9, 9, 9, 1, 4, 1, 10, 0, 0, 8]
-    )
-    assert result == find_maximal_subarray_sum(nums, k)
-
-
-def test_find_maximal_subarray_sum__k_more_len():
-    nums = [
-        -3,
-        -3,
-        -5,
-        -10,
-        -6,
-        -100,
-        2,
-        -1,
-        7,
-        -3,
-        9,
-        -3,
-        -2,
-        10,
-        -3,
-        7,
-        100,
-        -6,
-        9,
-        9,
-        9,
-        1,
-        4,
-        1,
-        10,
-        0,
-        0,
-        8,
-    ]
-
-    k = len(nums) * 2
-    result = sum(
-        [2, -1, 7, -3, 9, -3, -2, 10, -3, 7, 100, -6, 9, 9, 9, 1, 4, 1, 10, 0, 0, 8]
-    )
-    assert result == find_maximal_subarray_sum(nums, k)
+if __name__ == "__main__":
+    test_find_maximal_subarray_sum__init_data__end_subarray()
+    test_find_maximal_subarray_sum__middle_subarray()
+    test_find_maximal_subarray_sum__start_subarray()
