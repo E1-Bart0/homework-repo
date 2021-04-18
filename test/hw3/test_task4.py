dc48bae0162cc91545dccbff6fe574dc4c923168
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-04-18 10:55:07 +0300

    "renamed tests"

--- test/hw3/test_task4.py
+++ test/hw3/test_task4.py
@@ -1,19 +1,13 @@
 from All_home_works.hw3.task4 import is_armstrong
 
 
-def test_is_armstrong_case1():
+def test_is_armstrong__ok_153():
     assert is_armstrong(153)
 
 
-def test_is_armstrong_case2():
+def test_is_armstrong__not_ok_10():
     assert not is_armstrong(10)
 
 
-def test_is_armstrong_case3():
+def test_is_armstrong__ok_9():
     assert is_armstrong(9)
-
-
-if __name__ == "__main__":
-    test_is_armstrong_case1()
-    test_is_armstrong_case2()
-    test_is_armstrong_case3()
