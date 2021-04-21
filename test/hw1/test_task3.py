a807c7a9e69e8ada5e6ce04736d85b49717f01b0
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-04-21 10:37:14 +0300

    "With n^3 and n^2 solutions"

--- test/hw1/test_task3.py
+++ test/hw1/test_task3.py
@@ -1,15 +1,15 @@
-from All_home_works.hw1.task3 import check_sum_of_four
+from All_home_works.hw1.task3 import solution_with_n_in_2
 
 
 def test_check_sum_of_four():
     data = [[7, -7, -5], [-7, 5, -2], [-7, 0, -10], [-3, 0, 2]]
 
-    result = check_sum_of_four(*data)
+    result = solution_with_n_in_2(*data)
     assert 4 == result
 
 
 def test_check_sum_of_four_with_zeros():
     data = [[0, 0], [0, 0], [0, 0], [0, 0]]
 
-    result = check_sum_of_four(*data)
+    result = solution_with_n_in_2(*data)
     assert 2 ** 4 == result
