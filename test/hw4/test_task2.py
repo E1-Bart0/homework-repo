dc8020680ee5947a204610c3fc7c1d69621f4c43
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-04-28 10:09:27 +0300

    "add bad test"

--- test/hw4/test_task2.py
+++ test/hw4/test_task2.py
@@ -1,13 +1,15 @@
 from unittest.mock import patch
 
+import pytest
+
 from All_home_works.hw4.task2 import count_dots_on_i
 
 URL = "https://example.com/"
 
 
-class Data:
-    def __init__(self, data: list):
-        self.data = data
+class Response:
+    def __init__(self, data: str):
+        self.data = data.split(" ")
 
     def __enter__(self):
         return map(lambda x: x.encode(), self.data)
@@ -17,14 +19,20 @@ class Data:
 
 
 @patch("urllib.request.urlopen")
-def test_urlopen_got__path(urlopen):
+def test_urlopen__with_right_url(urlopen):
     count_dots_on_i(URL)
     urlopen.assert_called_with(URL)
+
+
+@patch("urllib.request.urlopen", side_effect=ValueError("BAD URL"))
+def test_urlopen__with_invalid_url():
+    with pytest.raises(ValueError, match="BAD URL"):
+        count_dots_on_i("BAD URL")
 
 
 @patch("urllib.request.urlopen")
 def test_count_dots_on_i__check_valid_data(urlopen):
     expect = 2
-    urlopen.return_value = Data(["<div></div>"])
+    urlopen.return_value = Response("<div> </div>")
     res = count_dots_on_i(URL)
     assert expect == res
