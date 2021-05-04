01c42e551fa6eb5aa05c795310d460e3fa8f0a38
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-05-04 14:20:24 +0300

    "fix hw4 test task3"

--- test/hw4/test_task3.py
+++ test/hw4/test_task3.py
@@ -1,28 +1,17 @@
 from All_home_works.hw4.task3 import my_precious_logger
 
-ERROR = "error: 404"
-TEXT = "OK: 200"
-
 
 def test_my_precious_logger__error_msg__test_msg_in_stderr(capsys):
-    my_precious_logger(text=ERROR)
+    error = "error: 404"
+    my_precious_logger(text=error)
     captured = capsys.readouterr()
-    assert captured.err == ERROR
-
-
-def test_my_precious_logger__error_msg__test_none_in_stdout(capsys):
-    my_precious_logger(text=ERROR)
-    captured = capsys.readouterr()
+    assert captured.err == error
     assert not captured.out
 
 
 def test_my_precious_logger__ok__msg__test_msg_in_stdout(capsys):
-    my_precious_logger(text=TEXT)
+    text = "OK: 200"
+    my_precious_logger(text=text)
     captured = capsys.readouterr()
-    assert captured.out == TEXT
-
-
-def test_my_precious_logger__ok_msg__test_none_in_stderr(capsys):
-    my_precious_logger(text=TEXT)
-    captured = capsys.readouterr()
+    assert captured.out == text
     assert not captured.err
