c1f6896f47db51c78f0a367127adf3bf7382a462
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-05-05 12:43:41 +0300

    "refactor"

--- test/hw3/test_task1.py
+++ test/hw3/test_task1.py
@@ -15,3 +15,26 @@ def test__cache_decorator():
 
     inner_function_with_decorator(1)
     assert inner_function.call_args_list == [call(1), call(2), call(1)]
+
+
+def test__check_the_result_of_the_function_calls():
+    inner_function = Mock()
+    inner_function_with_decorator = cache(times=2)(inner_function)
+    function_calls = []
+
+    inner_function_with_decorator(1)
+    function_calls.append(call(1))
+    assert inner_function.call_args_list == function_calls
+
+    inner_function_with_decorator(1)
+    assert inner_function.call_args_list == function_calls
+
+    inner_function_with_decorator(2)
+    function_calls.append(call(2))
+    assert inner_function.call_args_list == function_calls
+
+    inner_function_with_decorator(2)
+    assert inner_function.call_args_list == function_calls
+
+    inner_function_with_decorator(1)
+    assert inner_function.call_args_list == function_calls
