498303e108051bf19b2d7766a0f24c1dcb031e8a
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-05-05 12:13:24 +0300

    "hw7 task1"

---
+++ test/hw7/test_task1.py
@@ -0,0 +1,69 @@
+import pytest
+
+from All_home_works.hw7.task1 import find_occurrences
+
+
+@pytest.mark.parametrize(  # noqa: PT006
+    "input_data,element,expected",
+    [
+        ({"key1": "OK", "key2": "NOT OK"}, "OK", 1),
+        ({"key1": ["OK"], "key2": ["NOT OK"]}, "OK", 1),
+        ({"key1": ("OK",), "key2": ("NOT OK", "OK")}, "OK", 2),
+        ({"key1": {"OK"}, "key2": {"NOT OK"}}, "OK", 1),
+        ({"key1": {"key1": "OK"}, "key2": {"key1": "OK"}}, "OK", 2),
+        ({"key1": {"key1": ["OK"]}, "key2": {"key1": ("OK",)}}, "OK", 2),
+    ],
+)
+def test_find_occurrences__search_string(input_data, element, expected):
+    assert expected == find_occurrences(input_data, element)
+
+
+@pytest.mark.parametrize(  # noqa: PT006
+    "input_data,element,expected",
+    [
+        ({"key1": ["OK"], "key2": ["NOT OK"]}, ["OK"], 1),
+        ({"key1": ("OK",), "key2": ("NOT OK", "OK")}, ("OK",), 1),
+        ({"key1": {"OK"}, "key2": {"NOT OK"}}, {"OK"}, 1),
+        ({"key1": {"key1": ["OK"]}, "key2": {"key1": ("OK",)}}, ["OK"], 1),
+        (
+            {"key1": {"key1": ["OK"]}, "key2": {"key1": ("OK", {"key1": ["OK"]})}},
+            ["OK"],
+            2,
+        ),
+    ],
+)
+def test_find_occurrences__search__list__tuple__set(input_data, element, expected):
+    assert expected == find_occurrences(input_data, element)
+
+
+@pytest.mark.parametrize(  # noqa: PT006
+    "input_data,element,expected",
+    [
+        ({"key1": ["OK"]}, {"key1": ["OK"]}, 1),
+        ({"key1": ["OK", {"key1": "OK"}], "key2": ["NOT OK"]}, {"key1": "OK"}, 1),
+    ],
+)
+def test_find_occurrences__search__dict(input_data, element, expected):
+    assert expected == find_occurrences(input_data, element)
+
+
+def test_find_occurrences__many_occurrences__example_from_description():
+    input_data = {
+        "first": ["RED", "BLUE"],
+        "second": {
+            "simple_key": ["simple", "list", "of", "RED", "valued"],
+        },
+        "third": {
+            "abc": "BLUE",
+            "jhl": "RED",
+            "complex_key": {
+                "key1": "value1",
+                "key2": "RED",
+                "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
+            },
+        },
+        "fourth": "RED",
+    }
+    element = "RED"
+    expected = 6
+    assert expected == find_occurrences(input_data, element)
