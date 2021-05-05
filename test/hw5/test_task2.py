62e999b48aab58260f914a1a91ed093a6058b690
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-05-05 13:00:55 +0300

    "refactor"

--- test/hw5/test_task2.py
+++ test/hw5/test_task2.py
@@ -1,13 +1,18 @@
 from All_home_works.hw5.task2 import print_result
 
 
-def foo():
-    """Return str('foo')"""
-    return "foo"
+def foo(n):
+    """Takes n and return str('foo') n times"""
+    return "foo" * n
 
 
-def test_doc_string():
+def test_decorator_saving_doc_string_main_function():
     foo_with_decorator = print_result(foo)
-    assert foo_with_decorator.__doc__ == "Return str('foo')"
-    assert foo_with_decorator.__name__ == "foo"
+    assert foo_with_decorator.__doc__ == foo.__doc__
+    assert foo_with_decorator.__name__ == foo.__name__
     assert foo_with_decorator.__original_func == foo
+
+
+def test_function_works_fine_with_decorator():
+    foo_with_decorator = print_result(foo)
+    assert foo(2) == foo_with_decorator(2)
