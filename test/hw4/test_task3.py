57b1a80ec6372227642458b4df1085ee4c5108bc
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-05-06 14:11:16 +0300

    "test improvement"

--- test/hw4/test_task3.py
+++ test/hw4/test_task3.py
@@ -12,25 +12,17 @@ def test_my_precious_logger__error_msg__test_msg_in_stderr(capsys):
 
 
 @pytest.mark.parametrize("input_data", ["Error: 404", "error 404"])
-def test_my_precious_logger__error_msg_with_invalid_error_text_test_msg_in_stderr(
+def test_my_precious_logger__error_msg_with_invalid_error_text__test_msg_not_in_stderr(
     capsys, input_data
 ):
     my_precious_logger(text=input_data)
     captured = capsys.readouterr()
-    assert not captured.err
     assert input_data == captured.out
-
-
-def test_my_precious_logger__ok__msg__test_msg_in_stdout(capsys):
-    text = "OK: 200"
-    my_precious_logger(text=text)
-    captured = capsys.readouterr()
-    assert captured.out == text
     assert not captured.err
 
 
-def test_my_precious_logger__ok__msg_start_as_ordinary_msg__test_msg_in_stdout(capsys):
-    text = "test me"
+def test_my_precious_logger__ordinary_msg__test_msg_in_stdout(capsys):
+    text = "200"
     my_precious_logger(text=text)
     captured = capsys.readouterr()
     assert captured.out == text
