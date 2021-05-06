91741c19ed88919f943caa8516b0ca03fa02e1f6
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-05-06 14:05:45 +0300

    "test improvement"

--- test/hw3/test_task1.py
+++ test/hw3/test_task1.py
@@ -20,21 +20,21 @@ def test__cache_decorator():
 def test__check_the_result_of_the_function_calls():
     inner_function = Mock()
     inner_function_with_decorator = cache(times=2)(inner_function)
-    function_calls = []
 
     inner_function_with_decorator(1)
-    function_calls.append(call(1))
-    assert inner_function.call_args_list == function_calls
+    assert inner_function.call_args_list == [call(1)]
 
     inner_function_with_decorator(1)
-    assert inner_function.call_args_list == function_calls
+    assert inner_function.call_args_list == [call(1)]
 
     inner_function_with_decorator(2)
-    function_calls.append(call(2))
-    assert inner_function.call_args_list == function_calls
+    assert inner_function.call_args_list == [call(1), call(2)]
 
     inner_function_with_decorator(2)
-    assert inner_function.call_args_list == function_calls
+    assert inner_function.call_args_list == [call(1), call(2)]
 
     inner_function_with_decorator(1)
-    assert inner_function.call_args_list == function_calls
+    assert inner_function.call_args_list == [call(1), call(2)]
+
+    inner_function_with_decorator(1)
+    assert inner_function.call_args_list == [call(1), call(2), call(1)]
