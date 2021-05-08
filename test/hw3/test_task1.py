be7c70df843829d3671a46d8cca92197d7e79598
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-05-08 12:40:04 +0300

    "add test"

--- test/hw3/test_task1.py
+++ test/hw3/test_task1.py
@@ -38,3 +38,13 @@ def test__check_the_result_of_the_function_calls():
 
     inner_function_with_decorator(1)
     assert inner_function.call_args_list == [call(1), call(2), call(1)]
+
+
+def test__check_function_returns_correct_values():
+    @cache(times=2)
+    def foo(a, b):
+        return a + b
+
+    assert foo(1, 2) == 3
+    assert foo(1, 2) == 3
+    assert foo(2, 3) == 5
