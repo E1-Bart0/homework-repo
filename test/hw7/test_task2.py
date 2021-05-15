792653bb4a42b8cf3962a6c71d70fac40f225628
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-05-15 14:31:28 +0300

    "Hw11 task1 flake8 PT006"

--- test/hw7/test_task2.py
+++ test/hw7/test_task2.py
@@ -3,8 +3,8 @@ import pytest
 from All_home_works.hw7.task2 import backspace_compare
 
 
-@pytest.mark.parametrize(  # noqa: PT006
-    "first,second,expected",
+@pytest.mark.parametrize(
+    ("first", "second", "expected"),
     [
         ("aB#c", "aD#c", True),
         ("A##c", "#A#c", True),
@@ -17,8 +17,8 @@ def test__backspace_compare__with_backspace__expected_true(first, second, expect
     assert result
 
 
-@pytest.mark.parametrize(  # noqa: PT006
-    "first,second,expected",
+@pytest.mark.parametrize(
+    ("first", "second", "expected"),
     [("A#c", "b", False), ("##a", "c", False)],
 )
 def test__backspace_compare__with_backspace__expected_false(first, second, expected):
