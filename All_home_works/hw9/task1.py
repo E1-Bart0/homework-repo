654be2e1db67cb087c0c9fb6884bb63a2b602ac6
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-05-22 12:39:45 +0300

    "numbers__generators -> numbers_generators"

--- All_home_works/hw9/task1.py
+++ All_home_works/hw9/task1.py
@@ -16,27 +16,27 @@ from typing import Iterator, List, Union
 
 
 def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
-    numbers__generators = _get_numbers_and_generators(file_list)
+    numbers_generators = _get_numbers_and_generators(file_list)
 
-    while numbers__generators:
-        num__gen = min(numbers__generators, key=lambda x: x[0])
+    while numbers_generators:
+        num__gen = min(numbers_generators, key=lambda x: x[0])
         yield num__gen[0]
 
         next_number = next(num__gen[1], None)
         if next_number is None:
-            numbers__generators.remove(num__gen)
+            numbers_generators.remove(num__gen)
             continue
         num__gen[0] = next_number
 
 
 def _get_numbers_and_generators(file_list):
-    numbers__generators = []
+    numbers_generators = []
     for file in file_list:
         generator = _get_nums_from_file(file)
         first_num = next(generator, None)
         if first_num is not None:
-            numbers__generators.append([first_num, generator])
-    return numbers__generators
+            numbers_generators.append([first_num, generator])
+    return numbers_generators
 
 
 def _get_nums_from_file(filename):
