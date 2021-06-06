338b651c15f0ddb00ceb0c3d9c2b3f057c8b1f14
E1-Bart0 <starovoitov.vadik1@gmail.com>
2021-06-06 12:41:50 +0300

    "refactoring"

--- All_home_works/hw12/core/base.py
+++
@@ -1,2 +0,0 @@
-from All_home_works.hw12.core.db import Base
-from All_home_works.hw12.task_models import Homework, HomeworkResult, Student, Teacher
--- All_home_works/hw12/core/db.py
+++ All_home_works/hw12/core/db.py
@@ -1,6 +1,24 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 URL = "sqlite:///main.db"
 engine = create_engine(URL, echo=True)
 Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
