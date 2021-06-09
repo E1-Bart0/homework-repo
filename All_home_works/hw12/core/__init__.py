a660fd0125b6fdb50362148ae9df564889167d8e
E1-Bart0 <70700249+E1-Bart0@users.noreply.github.com>
2021-06-09 08:30:08 +0300

    Hw12 (#42)
    "HW 12 Task 1"

---
+++ All_home_works/hw12/core/__init__.py
@@ -0,0 +1,1 @@
+from All_home_works.hw12.core.db import URL, Base, Session, engine, session_scope
---
+++ All_home_works/hw12/core/base.py
@@ -0,0 +1,1 @@
+from All_home_works.hw12.task_models import Homework, HomeworkResult, Student, Teacher
